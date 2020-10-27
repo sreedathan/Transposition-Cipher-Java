f86def2f3bbe37c73500cf50e811675270b2d97a
sreedathan <sreedathang@gmail.com>
2020-10-28 02:13:34 +0530

    Sreedathan G Cryptography

---
+++ src/transcipher/mainofall.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transcipher;
+
+/**
+ *
+ * @author sreed
+ */
+public class mainofall {
+    public static void main(String args[])
+    {
+    NewJFrame1 form=new NewJFrame1();
+form.setVisible(true);
+    
+}
+}
+
